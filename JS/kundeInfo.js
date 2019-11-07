9199c5b3571513e1c8fd811149cb3fe9a979ad07
bjarne <bjarnewilhelmhaugen@hotmail.com>
2019-11-07 17:35:51 +0100

    lagt til mere kode på kundeInfo og ny bruker

---
+++ JS/kundeInfo.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        kunder: [
+          { kunde: 'mats' },
+          { kunde: 'Learn Vue' },
+          { kunde: 'Build something awesome' }
+        ]
+    }
+    }
+  );
