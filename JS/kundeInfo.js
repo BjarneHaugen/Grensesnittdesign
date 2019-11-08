93bf8121618cdcc3c3265b5448f7a970df5038a0
Bjarne Haugen <42610154+BjarneHaugen@users.noreply.github.com>
2019-11-08 22:02:20 +0100

    fikser new_user. og jobbet mere på kunde info

--- JS/kundeInfo.js
+++ JS/kundeInfo.js
@@ -1,11 +1,45 @@
+var kunde1 = {
+  fornavn: "Kåre",
+  etternavn: "Olsen",
+  nr: 1,
+  alder: 18,
+  addresse: "torshov, oslo"
+};
+
+var kunde2 = {
+  fornavn: "Trude",
+  etternavn: "Olsen",
+  nr: 2,
+  alder: 28,
+  addresse: "Strømsø, Drammen"
+};
+
+var kunde3 = {
+  fornavn: "Alex",
+  etternavn: "Olsen",
+  nr: 3,
+  alder: 23,
+  addresse: "Bislett, oslo"
+};
+
 var app = new Vue({
-    el: '#app',
-    data: {
-        kunder: [
-          { kunde: 'mats' },
-          { kunde: 'Learn Vue' },
-          { kunde: 'Build something awesome' }
-        ]
-    }
-    }
-  );
+  el: '#app',
+  data: {
+    kunder: [
+      { kunde: kunde1.fornavn + " " + kunde1.etternavn + ", kundenummer: " + kunde1.nr },
+      { kunde: kunde2.fornavn + " " + kunde1.etternavn + ", kundenummer: " + kunde2.nr },
+      { kunde: kunde3.fornavn + " " + kunde1.etternavn + ", kundenummer: " + kunde3.nr }
+    ]
+  }
+}
+);
+
+var app = new Vue({
+  el: '#app2',
+  data: {
+    kunder: [
+      { kunde: kunde1.fornavn + " " + kunde1.etternavn + ", kundenummer: " + kunde1.nr },
+    ]
+  }
+}
+);
