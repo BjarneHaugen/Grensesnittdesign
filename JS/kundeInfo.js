23f1c4bb44aada46f7df73b41c578bf55eb98d83
Bjarne Haugen <42610154+BjarneHaugen@users.noreply.github.com>
2019-11-09 21:34:52 +0100

    fikset på kundeinfo, og julian skal se på det

--- JS/kundeInfo.js
+++ JS/kundeInfo.js
@@ -1,44 +1,31 @@
-var kunde1 = {
-  fornavn: "Kåre",
-  etternavn: "Olsen",
-  nr: 1,
-  alder: 18,
-  addresse: "torshov, oslo"
-};
-
-var kunde2 = {
-  fornavn: "Trude",
-  etternavn: "Olsen",
-  nr: 2,
-  alder: 28,
-  addresse: "Strømsø, Drammen"
-};
-
-var kunde3 = {
-  fornavn: "Alex",
-  etternavn: "Olsen",
-  nr: 3,
-  alder: 23,
-  addresse: "Bislett, oslo"
-};
-
 var app = new Vue({
   el: '#app',
   data: {
     kunder: [
-      { kunde: kunde1.fornavn + " " + kunde1.etternavn + ", kundenummer: " + kunde1.nr },
-      { kunde: kunde2.fornavn + " " + kunde1.etternavn + ", kundenummer: " + kunde2.nr },
-      { kunde: kunde3.fornavn + " " + kunde1.etternavn + ", kundenummer: " + kunde3.nr }
-    ]
-  }
-}
-);
-
-var app = new Vue({
-  el: '#app2',
-  data: {
-    kunder: [
-      { kunde: kunde1.fornavn + " " + kunde1.etternavn + ", kundenummer: " + kunde1.nr },
+      {
+        nr: 1,
+        fornavn: "Kåre",
+        etternavn: "Olsen",
+        epost: "epost@epost.com",
+        img: "bilde.JPG",
+        mobil: 12345678
+      },
+      {
+        nr: 2,
+        fornavn: "Trude",
+        etternavn: "Hansen",
+        epost: "epost@epost.com",
+        img: "bilde.JPG",
+        mobil: 23456789
+      },
+      {
+        nr: 3,
+        fornavn: "Alex",
+        etternavn: "Nordmark",
+        epost: "epost@epost.com",
+        img: "bilde.JPG",
+        mobil: 34567890
+      },
     ]
   }
 }
