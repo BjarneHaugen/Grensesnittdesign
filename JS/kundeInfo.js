c7739b1fb101b53c4b0ef679461a5c4c8a40683d
Bjarne Haugen <42610154+BjarneHaugen@users.noreply.github.com>
2019-11-10 19:49:11 +0100

    fungerende i hermetegn kunde info side

--- JS/kundeInfo.js
+++ JS/kundeInfo.js
@@ -1,32 +1,38 @@
+var kunder = [
+  {
+    nr: 1,
+    fornavn: "Kåre",
+    etternavn: "Olsen",
+    epost: "epost@epost.com",
+    img: "bilde.JPG",
+    mobil: 12345678
+  },
+  {
+    nr: 2,
+    fornavn: "Trude",
+    etternavn: "Hansen",
+    epost: "epost@epost.no",
+    img: "bilde.JPG",
+    mobil: 23456789
+  },
+  {
+    nr: 3,
+    fornavn: "Alex",
+    etternavn: "Nordmark",
+    epost: "epost@epost.org",
+    img: "bilde.JPG",
+    mobil: 34567890
+  }
+];
+
+var selected = "";
+
+
 var app = new Vue({
   el: '#app',
   data: {
-    kunder: [
-      {
-        nr: 1,
-        fornavn: "Kåre",
-        etternavn: "Olsen",
-        epost: "epost@epost.com",
-        img: "bilde.JPG",
-        mobil: 12345678
-      },
-      {
-        nr: 2,
-        fornavn: "Trude",
-        etternavn: "Hansen",
-        epost: "epost@epost.com",
-        img: "bilde.JPG",
-        mobil: 23456789
-      },
-      {
-        nr: 3,
-        fornavn: "Alex",
-        etternavn: "Nordmark",
-        epost: "epost@epost.com",
-        img: "bilde.JPG",
-        mobil: 34567890
-      },
-    ]
+    kunder,
+    selected
   }
 }
 );
