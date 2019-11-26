61100deca15b4d9a3923f6102cf9c40c2b2989d2
Bjarne Haugen <42610154+BjarneHaugen@users.noreply.github.com>
2019-11-26 23:13:37 +0100

    mere oppdateringer

--- one_page/pages/About.page.js
+++ one_page/pages/About.page.js
@@ -1,73 +1,53 @@
 const AboutPage = {
     template: `
-    <div class="main-container">
-  
-         <!-- main container END -->
-        
-        <div class="sub-container">
-            <div class="Types-heading">
-                <h1> Studentklinikken </h1> </div>
-            <div class="border1"></div>
-            <div class="border2"></div>
-            <div class="border3">
-           <div class="border4">
-            <div class="Types-container">
-  
-                <!-- all borders and the types container END -->
-  
-                <div class="Types-box">
-                    <div class="service-name"></div>
-                    <h2>Type 1 </h2>
+    <div class="row">
+            <div class="col">
+
+                
+                <div class="type-desc">
+                        <h2>Type 1 </h2>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id facilisis quam, a
+                        pulvinar massa. Phasellus ullamcorper nibh sit amet eros dictum pretium.. </p>
                 </div>
-                <div class="type-desc"> 
-                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id facilisis quam, a pulvinar massa. Phasellus ullamcorper nibh sit amet eros dictum pretium.. </p>
+
+                
+                <div class="type-desc">
+                        <h2>Type 2 </h2>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id facilisis quam, a
+                        pulvinar massa. Phasellus ullamcorper nibh sit amet eros dictum pretium.. </p>
                 </div>
-            </div> <!-- TYPE 1 div closed -->
-  
-            <div class="Types-box">
-                  <div class="service-name"></div>
-                  <h2>Type 2 </h2>
-              </div>
-              <div class="type-desc"> 
-                  <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id facilisis quam, a pulvinar massa. Phasellus ullamcorper nibh sit amet eros dictum pretium.. </p>
-              </div>
-          </div> <!-- TYPE 2 div closed -->
-  
-          <div class="Types-box">
-                  <div class="type-name"></div>
-                  <h2>Type 3 </h2>
-              </div>
-              <div class="type-desc"> 
-                  <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id facilisis quam, a pulvinar massa. Phasellus ullamcorper nibh sit amet eros dictum pretium.. </p>
-              </div>
-          </div> <!-- TYPE 3 div closed -->
-  
-          <div class="Types-box">
-                  <div class="type-name"></div>
-                  <h2>Type 4 </h2>
-              </div>
-              <div class="type-desc"> 
-                  <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id facilisis quam, a pulvinar massa. Phasellus ullamcorper nibh sit amet eros dictum pretium.. </p>
-              </div>
-          </div> <!-- TYPE 4 div closed -->
-  
-          <div class="Types-box">
-                  <div class="type-name"></div>
-                  <h2>Åpningstider </h2>
-              </div>
-              <div class="type-desc"> 
-                  <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id facilisis quam, a pulvinar massa. Phasellus ullamcorper nibh sit amet eros dictum pretium.. </p>
-              </div>
-              </div>  <!-- TYPE 5 div closed -->
-  
-               <div class="Types-box">
-              <div class="type-name"></div>
-              <h2>Priser</h2>
-          </div>
-          <div class="type-desc"> 
-              <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id facilisis quam, a pulvinar massa. Phasellus ullamcorper nibh sit amet eros dictum pretium.. </p>
-          </div>
-          </div>
+
+                
+                <div class="type-desc">
+                        <h2>Type 3 </h2>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id facilisis quam, a pulvinar
+                        massa. Phasellus ullamcorper nibh sit amet eros dictum pretium.. </p>
+                </div>
+
+               
+                <div class="type-desc">
+                        <h2>Type 4 </h2>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id facilisis quam, a pulvinar
+                        massa. Phasellus ullamcorper nibh sit amet eros dictum pretium.. </p>
+                </div>
+
+                
+                <div class="type-desc">
+                        <h2>Åpningstider </h2>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id facilisis quam, a pulvinar
+                        massa.
+                        Phasellus ullamcorper nibh sit amet eros dictum pretium.. </p>
+                </div>
+
+               
+                <div class="type-desc">
+                        <h2>Priser</h2>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id facilisis quam, a pulvinar
+                        massa.
+                        Phasellus ullamcorper nibh sit amet eros dictum pretium.. </p>
+                </div>
+            </div>
+        </div>
 `
 };
 
