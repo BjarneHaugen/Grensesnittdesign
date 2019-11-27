8c75e22647c99b7938aa71583ea4b32c52328c6c
mats <mats1@live.no>
2019-11-28 00:46:26 +0100

    add styling and more lines to contact and home

--- one_page/pages/Contact.page.js
+++ one_page/pages/Contact.page.js
@@ -2,26 +2,63 @@ const ContactPage = {
     template: `
         <div>
             <toolbar></toolbar>
-                <div class="test">
-                    <h1>Kontakt oss</h1>
-                    <form>
-                        <input v-model="contactForm.name" placeholder="Navn" type="text" /><br>
-                        <input v-model="contactForm.email" placeholder="Epost-adresse" type="text" /><br>
-                        <textarea v-model="contactForm.message" placeholder="Din melding" /><br>
-                        <button type="button" @click="sendMail">Send mail!</button>
-                    </form>
+                
+            
+
+            <div class="container">
+            <dic class="row">
+            <div class="col-4"></div>
+            <div class="col-4">
+                <div class="col-4"> Fornavn</div>
+                <input v-model="contactForm.name" type="text" placeholder="Skriv ditt fornavn her">
+                <div class="fornavn"> </div>
+
+                <div class="col"> Etternavn</div>
+                <input v-model="contactForm.etternavn"type="text" placeholder="Skriv ditt etternavn her">
+                <div class="etternavn"> </div>
+
+                <div class="col-4"> E-Post </div>
+                <input v-model="contactForm.email" type="text" placeholder="Skriv din e-post her">
+                <div class="epost"> </div>
+
+                <p>Ønsker du å være anonym?</p>
+                <select id="anonym" name="anonym">
+                    <option value="Empty"></option>
+                    <option value="Ja">Ja</option>
+                    <option value="Ja">Nei</option>
+                </select>
+                <div closed></div>
+
+                <div class="col"> Tilbakemelding  
+                    
+                <textarea v-model="contactForm.message" id="Tilbakemelding-boks" cols="40" rows="5"></textarea>
+                
+                </div>
+                
+                <div class="row">
+                <div class="col-4"></div>
+                <div class="col-4">
+                    <button type="button" @click="sendMail">Send mail!</button>
+                </div>
+                <div class="col-4"></div>
                 </div>
             </div>
+            <div class="col-4"></div>
+            
+        <div id="filler"></div>
+        </div>
+        </div>
                 `,
     methods: {
         sendMail() {
-            alert(`Melding sent fra ${this.contactForm.name} (${this.contactForm.email}): "${this.contactForm.message}"`);
+            alert(`Melding sent fra: \n navn: ${this.contactForm.name} ${this.contactForm.etternavn} \n email: ${this.contactForm.email} \n message: "${this.contactForm.message}"`);
         }
     },
     data() {
         return {
             contactForm: {
                 name: "",
+                etternavn: "",
                 email: "",
                 message: ""
             }
