04170ac7f92b9bc93bfeaa1ae07d5a7ae23d23f9
bjarne <bjarnewilhelmhaugen@hotmail.com>
2019-11-27 19:25:04 +0100

    la opp onepage grid i index.html. fiksa litt i my page

--- one_page/components/toolbar.component.js
+++ one_page/components/toolbar.component.js
@@ -3,21 +3,61 @@ const Toolbar = Vue.component('Toolbar', {
             <div class="row" id="headerRow">
 
                 <div class="col-5" id="mainpage">
-                    <router-link to="/" class="TB"> Student klinikken </router-link>
+
+                    <router-link to="/"> 
+
+                        <div>
+                        
+                        Student klinikken 
+                        
+                        </div>
+
+                    </router-link>
+
                 </div>
 
                 <div class="col-2" id="om-oss">
-                  <router-link to="/about" class="about"> om oss </router-link>
+
+                  <router-link to="/about" class="about"> 
+
+                    <div>
+                       
+                    om oss 
+                  
+                    </div>
+
+                  </router-link>
+
                 </div>
+
                 <div class="col-2" id="timebestilling">
-                    <router-link to="/timebestilling" class="TB">bestilling time</router-link>
+
+                    <router-link to="/timebestilling" class="TB">
+                    <div>
+                    bestilling time
+                    </div>
+                    </router-link>
+
                 </div>
+
                 <div class="col-2" id="tilbakemelding"> 
-                    <router-link to="/contact" class="TB"> tilbakemelding </router-link>
+                
+                    <router-link to="/contact" class="TB"> 
+                    <div>
+                    tilbakemelding 
+                    </div>
+                    </router-link>
+
                 </div>
                
                 <div class="col-1" id="min-side">
-                    <router-link to="/my_page" class="TB"> min side </router-link>
+
+                    <router-link to="/my_page" class="TB"> 
+                    <div>
+                    min side 
+                    </div>
+                    </router-link>
+
                 </div>
             </div>
              `
--- one_page/pages/my_page.page.js
+++ one_page/pages/my_page.page.js
@@ -3,11 +3,34 @@ const My_page = {
             <div>
                 <toolbar></toolbar>
                     <div>
-                        min side
+                       din time hos {{apointments.type}}
+                    </div>
+                    <div>
+                        dato {{apointments.date}} {{apointments.month}} kl {{apointments.time}} 
+                    </div>
+                    <div>
+                    behandlere {{apointments.worker}}
                     </div>
             </div>
             `,
+    methods: {
+        removeDate() {
+            ;
+        }
+    },
+    data() {
+        return {
+            apointments:
+            {
+                type: "akupunktur",
+                month: "mai",
+                date: 10,
+                time: 12.15,
+                worker: "lasse"
+            }
 
+        }
+    }
 };
 
 export default My_page;
