ea111cad6400284f5274ae989fbaa9311fc951b7
mats <mats1@live.no>
2019-11-28 21:50:42 +0100

    reeee

--- one_page/pages/Home.page.js
+++ one_page/pages/Home.page.js
@@ -10,29 +10,36 @@ const HomePage = {
                
                 <div class="col-md-6">
                     <div id="bilde-placeholder"></div>
-                </div>
-                <div class="col-3"></div>
-                <div class="col-3">
-                    <div class="login-signup">log in</div>
-                    <div class="login-signup">sign up</div>
-                </div>
-            </div>
-      
-            <div class="row">
+
+                    <div class="row">
                 <div id="filler"></div>
             </div>
             <div class="row">
-                <div class="col-md-6 leftText">
+                <div class="col leftText">
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id officia eaque, natus laborum rem facilis dolorum et hic soluta veritatis voluptates doloremque velit. Nisi, maxime necessitatibus corrupti quidem fuga voluptate.
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit suscipit nisi adipisci, possimus optio animi voluptatem molestias. Officiis libero rem commodi magnam ipsa recusandae esse est natus incidunt. Unde, inventore!</p></div>
-                <div class="col-2">
-                    <div id="thatbutton">se behandlinger</div>
+                
+            </div>
                 </div>
-                <div class="col-2"></div>
-                <div class="col-2">
-                    <div id="thisbutton">bestill time</div>
+
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="col-md-6 roundedBox linkButton">se behandlinger</div>
+
+                        <div class="col-md-6 roundedBox linkButton">bestill time</div>
+                    </div>
+
+                    <div class="row"><div class="filler"></div></div>
+
+                    <div class="row">
+                        <div class="col-md-6 roundedBox linkButton">log in</div>
+                        <div class="col-md-6 roundedBox linkButton">sign up</div>
+                    </div>
+
                 </div>
             </div>
+      
+            
         </div> 
     </div>
 `,
