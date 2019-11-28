e67562773ed7adcc26c7c1ddcc26e09e7ccc58f9
bjarne <bjarnewilhelmhaugen@hotmail.com>
2019-11-28 19:39:30 +0100

    mere rot

--- one_page/pages/timebestilling.page.js
+++ one_page/pages/timebestilling.page.js
@@ -14,7 +14,13 @@ const Timebestilling = {
                     <div>
 
                         <div v-for="hour in tid" v-bind:value="hour">
-                            {{hour.time}} : {{hour.min}}
+                            <div v-show="hour.bestilt===true"> bestilt </div>
+                            <div v-else> 
+                                <div>   
+                                    {{hour.time}} : {{hour.min}}  
+                                    <button @click="bestilling()"> bestill </button>
+                                </div> 
+                            </div>
                         </div>
 
                     </div>
@@ -22,7 +28,12 @@ const Timebestilling = {
 
             </div>
             `,
-
+    methods: {
+        bestilling: function () {
+            this.$set(this.tid, this.tid.bestilt = !false)
+            console.log("ok " + this.tid.bestilt);
+        }
+    },
 
     data() {
         return {
@@ -30,24 +41,240 @@ const Timebestilling = {
 
             tid: [
                 {
-                    time: 9,
-                    min: "00"
+                    time: 7,
+                    min: "30",
+                    bestilt: false
+                },
+                {
+                    time: 7,
+                    min: "45",
+                    bestilt: false
+                },
+                {
+                    time: 8,
+                    min: "00",
+                    bestilt: false
+                },
+                {
+                    time: 8,
+                    min: "15",
+                    bestilt: false
+                },
+                {
+                    time: 8,
+                    min: "30",
+                    bestilt: false
+                },
+                {
+                    time: 8,
+                    min: "45",
+                    bestilt: false
                 },
                 {
                     time: 9,
-                    min: 15
-                }, 
-                {
-                    time: 9,
-                    min: 30
+                    min: "00",
+                    bestilt: false
                 },
                 {
                     time: 9,
-                    min: 45
+                    min: "15",
+                    bestilt: false
+                },
+                {
+                    time: 9,
+                    min: "30",
+                    bestilt: false
+
+                },
+                {
+                    time: 9,
+                    min: "45",
+                    bestilt: false
                 },
                 {
                     time: 10,
-                    min: "00"
+                    min: "00",
+                    bestilt: false
+                },
+                {
+                    time: 10,
+                    min: "15",
+                    bestilt: false
+                },
+                {
+                    time: 10,
+                    min: "30",
+                    bestilt: false
+                },
+                {
+                    time: 10,
+                    min: "45",
+                    bestilt: false
+                },
+                {
+                    time: 11,
+                    min: "00",
+                    bestilt: false
+                },
+                {
+                    time: 11,
+                    min: "15",
+                    bestilt: false
+                },
+                {
+                    time: 11,
+                    min: "30",
+                    bestilt: false
+                },
+                {
+                    time: 11,
+                    min: "45",
+                    bestilt: false
+                },
+                {
+                    time: 12,
+                    min: "00",
+                    bestilt: false
+                },
+                {
+                    time: 12,
+                    min: "15",
+                    bestilt: false
+                },
+                {
+                    time: 12,
+                    min: "30",
+                    bestilt: false
+                },
+                {
+                    time: 12,
+                    min: "45",
+                    bestilt: false
+                },
+                {
+                    time: 13,
+                    min: "00",
+                    bestilt: false
+                },
+                {
+                    time: 13,
+                    min: "15",
+                    bestilt: false
+                },
+                {
+                    time: 13,
+                    min: "30",
+                    bestilt: false
+                },
+                {
+                    time: 13,
+                    min: "45",
+                    bestilt: false
+                },
+                {
+                    time: 14,
+                    min: "00",
+                    bestilt: false
+                },
+                {
+                    time: 14,
+                    min: "15",
+                    bestilt: false
+                },
+                {
+                    time: 14,
+                    min: "30",
+                    bestilt: false
+                },
+                {
+                    time: 14,
+                    min: "45",
+                    bestilt: false
+                },
+                {
+                    time: 15,
+                    min: "00",
+                    bestilt: false
+                },
+                {
+                    time: 15,
+                    min: "15",
+                    bestilt: false
+                },
+                {
+                    time: 15,
+                    min: "30",
+                    bestilt: false
+                },
+                {
+                    time: 15,
+                    min: "45",
+                    bestilt: false
+                },
+                {
+                    time: 16,
+                    min: "00",
+                    bestilt: false
+                },
+                {
+                    time: 16,
+                    min: "15",
+                    bestilt: false
+                },
+                {
+                    time: 16,
+                    min: "30",
+                    bestilt: false
+                },
+                {
+                    time: 16,
+                    min: "45",
+                    bestilt: false
+                },
+                {
+                    time: 17,
+                    min: "00",
+                    bestilt: false
+                },
+                {
+                    time: 17,
+                    min: "15",
+                    bestilt: false
+                },
+                {
+                    time: 17,
+                    min: "30",
+                    bestilt: false
+                },
+                {
+                    time: 17,
+                    min: "45",
+                    bestilt: false
+                },
+                {
+                    time: 18,
+                    min: "00",
+                    bestilt: false
+                },
+                {
+                    time: 18,
+                    min: "15",
+                    bestilt: false
+                },
+                {
+                    time: 18,
+                    min: "30",
+                    bestilt: false
+                },
+                {
+                    time: 18,
+                    min: "45",
+                    bestilt: false
+                },
+                {
+                    time: 19,
+                    min: "00",
+                    bestilt: false
                 }
             ]
         }
