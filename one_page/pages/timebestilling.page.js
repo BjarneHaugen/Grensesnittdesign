f29adbc2a298e21b9b78fe5f84cb502dd3c48e00
bjarne <bjarnewilhelmhaugen@hotmail.com>
2019-11-28 13:47:02 +0100

    fikser timebestilling så man får dato

--- one_page/pages/timebestilling.page.js
+++ one_page/pages/timebestilling.page.js
@@ -3,11 +3,23 @@ const Timebestilling = {
             <div>
                 <toolbar></toolbar>
                     <div>
-                        test
+
+                        <input type='date' v-model='myDate'>
+
                     </div>
+
+                    {{myDate}}
+
+                    
             </div>
             `,
 
+
+    data() {
+        return {
+            myDate: new Date().toISOString().slice(0, 10)
+        }
+    }
 };
 
 export default Timebestilling;
