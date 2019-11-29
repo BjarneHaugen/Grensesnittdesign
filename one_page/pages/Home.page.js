e37562fbce0ec184106e9434803bffce731b52ae
PieNappl3 <mats1@live.no>
2019-11-29 04:31:22 +0100

    theres images now

--- one_page/pages/Home.page.js
+++ one_page/pages/Home.page.js
@@ -8,13 +8,13 @@ const HomePage = {
         </div>
             <div class="row">
                
-                <div class="col-md-6">
-                    <div id="bilde-placeholder"></div>
+                <div class="col-md-12">
+                    <div id="bilde-placeholder">
+                        <img src="./resources/clinic.jpg" alt="a clinic">
+                    </div>
 
-                    <div class="row">
-                <div id="filler"></div>
-            </div>
             <div class="row">
+            <div class="col-md-6">
                 <div class="col leftText">
                     <p>Vi behandler akutte og langvarige plager / ubehag, men kan også hjelpe til med forebyggende tiltak. Du kan forvente en individuell samtale, undersøkelse, behandling og råd rettet mot dine behov.
 
@@ -22,24 +22,30 @@ const HomePage = {
                     
                     Du trenger ikke henvisning fra lege.</p>
                     </div>
-                
-            </div>
                 </div>
-
                 <div class="col-md-6">
-                    <div class="row">
-                        <div class="col-md-6 roundedBox linkButton">se behandlinger</div>
+                <div style="height: 2.5vh"></div>
+                <div class="row">
 
-                        <div class="col-md-6 roundedBox linkButton">bestill time</div>
-                    </div>
-
-                    <div class="row"><div class="filler"></div></div>
-
-                    <div class="row">
-                    
                         <div class="col-md-6 roundedBox linkButton">
+                        <router-link to="/about" class="about">
+                        <div style="color: black">
+                        se behandlinger
+                        </div>
+                        </router-link>
+                        </div>
 
-
+                        
+                        <div class="col-md-6 roundedBox linkButton">
+                        <router-link to="/timebestilling" class="timebestilling">
+                        <div style="color: black">
+                        bestill time
+                        </div>
+                        </router-link>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 roundedBox linkButton">
                         <router-link to="/loginchoice" class="loginchoice"> 
                         <div class="hundredpercent" style="color: black">
                             log in
@@ -48,9 +54,12 @@ const HomePage = {
                         </div>
                         <div class="col-md-6 roundedBox linkButton">sign up</div>
                     </div>
+                </div>
 
+            </div>
                 </div>
             </div>
+            
       
             
         </div> 
